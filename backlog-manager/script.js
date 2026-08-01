b07b31b7464253f8e9ed4cc1ec66be8abec70dd4
Mukesh Gupta <gmukesh.work@gmail.com>
2026-08-01 14:19:41 +0530

    Build first working backlog manager

---
+++ backlog-manager/script.js
@@ -0,0 +1,24 @@
+const backlogItems = [];
+
+function addItem() {
+    const input = document.getElementById("backlogInput");
+    const list = document.getElementById("backlogList");
+    const item = document.createElement("li");
+
+    if (input.value === "") {
+        return;
+    }
+
+    backlogItems.push(input.value);
+
+    item.textContent = input.value;
+    list.appendChild(item);
+    input.value = "";
+
+    item.onclick = function () {
+        const confirmed = confirm("Are you sure?");
+        if (confirmed) {
+            item.remove();
+        }
+    }
+}
